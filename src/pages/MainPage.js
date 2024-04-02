90d3ca5fd95a3cac55fcded34374c9ac38f7fb62
SanudaKJ <sanudajayasinghe18@gmail.com>
2024-04-02 10:48:53 +0530

    feat: Folder Structure

---
+++ src/pages/MainPage.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1 className="text-5xl lg:mx-32 font-bold text-green-500">
+        Convert Your Currencies
+      </h1>
+      <p className="lg:mx-32 text-2xl opacity-40 pt-5">
+        Check live rates, send money securely, set rate alerts, receive
+        notifications and more.
+      </p>
+    </div>
+  );
+}
